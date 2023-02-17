d3ee15ce9132f642858fccd3a192e535d240a3fc
oolufolabii <otitoola.olufolabi@gmail.com>
2023-02-17 17:15:00 +0100

    Pagination Tasks

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -50,13 +50,13 @@ class Server:
         """Returns information/data from a page"""
 
         query_data = self.get_page(page, page_size)
-        first_index, last_index = index_range(page, page_size)
+        first_index, last_idx = index_range(page, page_size)
         total_pages = math.ceil(len(self.__dataset)/page_size)
         page_data_info = {
             'page_size': len(query_data),
             'page': page,
             'data': query_data,
-            'next_page': page + 1 if last_index < len(self.__dataset) else None,
+            'next_page': page + 1 if last_idx < len(self.__dataset) else None,
             'prev_page': page - 1 if first_index > 0 else None,
             'total_pages': total_pages,
         }
