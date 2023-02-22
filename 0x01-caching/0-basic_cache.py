8abb7ed59d6518f4b2064ecd5084fdd2eb6db51f
oolufolabii <otitoola.olufolabi@gmail.com>
2023-02-22 11:43:33 +0100

    Caching Tasks

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Python Module on Simple Caching"""
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """An object to create, store and retrieve data from dictionary"""
+
+    def put(self, key, item):
+        """Adds data to the cache"""
+        if key is None and item is None:
+            return
+        self.cache_data[key] = item
+    
+    def get(self, key):
+        """Returns data from the cache"""
+        if key is None:
+            return
+        return self.cache_data.get(key, None)
