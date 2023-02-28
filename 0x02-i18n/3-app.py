612d986f8c3cbb87546152cd430c69cc9bdf8109
oolufolabii <otitoola.olufolabi@gmail.com>
2023-02-28 12:57:18 +0100

    i18n Tasks

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,7 +2,7 @@
 """A Flask app.
 """
 from flask_babel import Babel
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 class Config:
@@ -21,11 +21,18 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
 @app.route('/')
 def index_page() -> str:
     """The index page.
     """
-    return render_template('1-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
